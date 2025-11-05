7e30c29614cca78c33211a718a8f54ca88ade358
Vibhavari Vijaykumar Borade <vibhavarib@cybage.com>
2025-11-05 15:14:46 +0530

    added get and post methods

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,5 +1,39 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Product;
+import com.example.demo.service.ProductService;
+
+@RestController
+@RequestMapping("/productService")
 public class ProductController {
+
+	@Autowired
+	private ProductService productService;
+
+	@GetMapping
+	public List<Product> getAllProduct() {
+		List<Product> productList = productService.getProductList();
+		return productList;
+	}
+
+	@PostMapping("/addProduct")
+	public ResponseEntity<Product> addProduct(@RequestBody Product product) {
+		Product productSaved = productService.saveProduct(product);
+		return  new ResponseEntity<Product>(productSaved,HttpStatus.OK);
+
+		
+	}
 
 }
--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -1,5 +1,27 @@
 package com.example.demo.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Product;
+import com.example.demo.repository.ProductRepository;
+
+@Service
 public class ProductService {
+	
+	@Autowired
+	private ProductRepository productRepository;
+
+	public List<Product> getProductList() {
+		List<Product> listProduct=productRepository.findAll();
+		return listProduct;
+	}
+
+	public Product saveProduct(Product product) {
+		Product savedProduct=productRepository.save(product);
+		return savedProduct;
+	}
 
 }
